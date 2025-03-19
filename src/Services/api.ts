999229efbe44cb7cddb90a7fb1ed64698457e761
mohamed amine hamdi <aminehamdi443@gmail.com>
2025-03-19 15:22:20 +0100

    Step 1 : Adding comments

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = 'DEMO_KEY';
+// Base URL for NASA's Near Earth Object (NEO) API
 const BASE_URL = 'https://api.nasa.gov/neo/rest/v1/neo/browse';
 
 export const fetchNEOData = async () => {
+  
+  // Send a GET request to the NASA API to retrieve NEO data
   const response = await axios.get(`${BASE_URL}?api_key=${API_KEY}`);
   return response.data;
 };
